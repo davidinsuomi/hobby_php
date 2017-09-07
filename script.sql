1583ad747979891ae2e053667e9a4153a57b364f
weiding <davidinsuomi@gmail.com>
2017-09-07 22:41:05 +0800

    add few functions

---
+++ script.sql
@@ -0,0 +1,3 @@
+CREATE TABLE omg_tbl(num_of_address INT, time_stamp TIMESTAMP );
+CREATE TABLE qtum_tbl(num_of_address INT, time_stamp TIMESTAMP );
+CREATE TABLE golem_tbl(num_of_address INT, time_stamp TIMESTAMP );
